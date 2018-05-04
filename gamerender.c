f8beb05e30f50aa4c0679a238ae1a0f5a1bee9e4
infval <38145742+infval@users.noreply.github.com>
2018-05-04 17:31:00 +0300

    Remove warning, formatting code

--- gamerender.c
+++ gamerender.c
@@ -6,22 +6,19 @@
 #include "resmap.h"
 #include "map.h"
 
-u16 water_strobe = 0;
+static u16 water_strobe = 0;
 static const s16 bullet_sprite_offset_x[4] = { 3+0, -4, 3+0, -3 };
 static const s16 bullet_sprite_offset_y[4] = {  -8,  0,  -7,  0 };
 
 void GRend_prepareRepaint() {
 
-    u16 i;
-    u16 x;
-    u16 y;
-    //u16 r;
+    u16 i, x, y;
 
     if (pause && (water_strobe & 16)) {
         x = (MAP_W >> 1 << 3) - 20;
         y = (MAP_H >> 1 << 3) + 1;
-        drawSprite4x1(SPRITE_PAUSE | TILE_ATTR(0, 1, 0, 0), x, y);           // PAUS
-        drawSprite1x1(SPRITE_PAUSE | TILE_ATTR(0, 1, 0, 0) + 4, x + 8*4, y); // E
+        drawSprite4x1(SPRITE_PAUSE | TILE_ATTR(0, 1, 0, 0), x, y);             // PAUS
+        drawSprite1x1((SPRITE_PAUSE + 4) | TILE_ATTR(0, 1, 0, 0), x + 8*4, y); // E
     }
 
     if (scor.timer) {
@@ -33,7 +30,7 @@ void GRend_prepareRepaint() {
     }
 
     for (i = 0; i < config.max_bullets; i++) {
-        if (!bullets[i].speed)continue;
+        if (!bullets[i].speed) continue;
         drawSprite1x2(SPRITE_ADDR_BULLET + (bullets[i].rotate << 1),
             bullets[i].posx + bullet_sprite_offset_x[bullets[i].rotate],
             bullets[i].posy + bullet_sprite_offset_y[bullets[i].rotate]);
@@ -41,7 +38,7 @@ void GRend_prepareRepaint() {
 
     i = config.max_explode;
     while (i--) {
-        if (!explodes[i].type)continue;
+        if (!explodes[i].type) continue;
         if (explodes[i].ani_counter > 2) {
 
             drawSprite8x8(SPRITE_EXPLODE_BIG + ((explodes[i].ani_counter - 3) << 4), explodes[i].posx - 8, explodes[i].posy - 8);
@@ -58,26 +55,19 @@ void GRend_prepareRepaint() {
                 explodes[i].strobe = 0;
             }
         }
-        if (explodes[i].type == EXPLODE_BIG && explodes[i].ani_counter > 4)explodes[i].type = 0;
-        if (explodes[i].type == EXPLODE_SMALL && explodes[i].ani_counter > 2)explodes[i].type = 0;
+        if (explodes[i].type == EXPLODE_BIG   && explodes[i].ani_counter > 4) explodes[i].type = 0;
+        if (explodes[i].type == EXPLODE_SMALL && explodes[i].ani_counter > 2) explodes[i].type = 0;
         explodes[i].strobe++;
 
     }
-
 
     for (i = 0; i < config.units_on_map; i++) {
         drawTank(&game_player[i]);
     }
 
-
     if (bonus.type) {
         drawBonus(&bonus);
     }
-
-
-
-
-
 }
 
 void GRend_repaint() {
@@ -89,7 +79,7 @@ void GRend_repaint() {
 		VDP_setPaletteColor(/*0,*/ RES_COLOR_WATER3_ADDR, pal_red[RES_COLOR_WATER2_ADDR]);
 	}
     if ((water_strobe & 63) == 31) {
-		VDP_setPaletteColor(/*0,*/RES_COLOR_WATER3_ADDR, pal_red[RES_COLOR_WATER1_ADDR]);
+		VDP_setPaletteColor(/*0,*/ RES_COLOR_WATER3_ADDR, pal_red[RES_COLOR_WATER1_ADDR]);
         VDP_setPaletteColor(/*0,*/ RES_COLOR_WATER2_ADDR, pal_red[RES_COLOR_WATER2_ADDR]);
     }
 }
